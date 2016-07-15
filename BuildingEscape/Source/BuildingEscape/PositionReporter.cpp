5d8d5533308b9ee4193f9e0ec35dadeb772c9171
Malik Gray <mikey@starfantasygames.com>
2016-07-15 01:33:44 -0400

    Added logging to display the name of the current object that the component is attached to.

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -21,8 +21,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report, reporting for %s"), *ObjectName);
 }
 
 
